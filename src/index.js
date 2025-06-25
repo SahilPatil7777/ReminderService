71b272abfc30f0d27042b32128c9c83e21718532
Sahil Patil <sahilpatil32267@gmail.com>
2025-06-25 09:26:25 +0530

    Setup of smtp using nodemailer is Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { PORT } = require('./config/serverConfig');
+const { PORT } = require("./config/serverConfig");
+
+const { sendBasicEmail } = require("./services/email-service");
 
 const setupAndStartServer = () => {
   const app = express();
@@ -10,6 +12,12 @@ const setupAndStartServer = () => {
 
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "sahilpatil.work7777@gmail.com",
+    //   "Congratulations! You're Selected for the Software Tester Position",
+    //   "Hey how are you doing? I hope you are having a great day!"
+    // );
   });
 };
 
